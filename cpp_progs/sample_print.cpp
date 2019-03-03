183609302c607703ba015465b40f0664ffc3f9a1
Venkat Thogaru <rajthogarunagaraj@gmail.com>
2019-03-03 11:23:35 +0530

    DO: sample print

---
+++ cpp_progs/sample_print.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+int num1, num2;
+cout <<"Enter num1"<<endl;
+cin>>num1;
+cout <<"Enter num2"<<endl;
+cin>>num2;
+
+cout<<"greater number is"<<((num1>num2)?num1:num2)<<endl;
+
+
+}
